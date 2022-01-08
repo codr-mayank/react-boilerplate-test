04e4f4a40f768e5bc04a4b2ade90a5b7bf47e849
Mayank Porwal <mayank.porwal@rivigo.com>
2022-01-09 00:10:14 +0530

    sonar changes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './app/App';
 import './main.scss';
 
-const data = {};
-const func = () => {
-  // demo function
-};
-
 /**
  * ReactDOM.render: Renders App to index html file
  */
-ReactDOM.render(<App sampleData={data} sampleProp={func} />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
