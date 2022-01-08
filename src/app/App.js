0668149ec82f63719e2198cf14df01fd506cfb57
Mayank Porwal <mayank.porwal@rivigo.com>
2022-01-08 21:35:35 +0530

    react boilerplate setup

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  console.log('App executed');
+  console.log('no errors');
+  console.log('no errors');
+
+  return <h1 className="helloWorld">Hello World!</h1>;
+};
+
+export default App;
