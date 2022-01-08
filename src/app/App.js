fac33710534903690532ac4c881559528214994b
Mayank Porwal <mayank.porwal@rivigo.com>
2022-01-08 21:53:40 +0530

    husky setup

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,7 @@
-import React from 'react';;
-import hjshdsjk from 'syhjknkx';
+import React from 'react';
 
 const App = () => {
-  console.log('App executed');
-  console.log('no errors');
-  console.log('no errors');
-
-  return <h1 className="helloWorld">Hello World!
-  
-  </h1>;
+  return <h1 className="helloWorld">Hello World!</h1>;
 };
 
 export default App;
